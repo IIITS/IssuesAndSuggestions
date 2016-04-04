e87b7dd52b74cd434b0e964af5f4b4adceda6b31
sahalsajjad <sahalsajjad@gmail.com>
2016-04-04 22:35:28 +0530

    LoginURL Fixed

--- gp/urls.py
+++ gp/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 urlpatterns = [
 			url(r'^complaint/post/$',PostComplaint.as_view(),name = 'postcomplaint'),
-			url(r'^$',LoginView.as_view(),name = 'login'),
+			url(r'^accounts/login/$',LoginView.as_view(),name = 'login'),
 			url(r'^complaints/view/$',ViewComplaintByDomain.as_view(),name= 'complaints'),
 			url(r'^accounts/home/$',HomeView.as_view(),name='homepage'),
 			url(r'^upvote/complaint/$',Upvotes,name = 'upvote'),
