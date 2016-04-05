e0f6e5421a13f53a256fdb1c7d9b08893ec3463d
tbhavana <bhavanatangirala25@gmail.com>
2016-04-05 15:58:42 +0530

    added chips

--- gp/views.py
+++ gp/views.py
@@ -155,8 +155,4 @@ def Upvotes(request):
 	 
 	 
 def homeRedirect(request):
-	HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
-			
-
-	 
-
+	return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
